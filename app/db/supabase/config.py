c726cf2e93fa796f85121d55872ff6de8301488c
JEFERSSON_QE <112734981+Manzanito20003@users.noreply.github.com>
2025-12-08 20:00:01 -0500

    [fix] error for coenct supabase

--- app/db/supabase/config.py
+++ app/db/supabase/config.py
@@ -1,17 +1,26 @@
 from supabase import create_client, Client
-from dotenv import load_dotenv
-import os
 from app.core.config import settings
+from typing import Optional
 
-load_dotenv()
+
 
 SUPABASE_URL = settings.SUPABASE_URL
 SUPABASE_KEY = settings.SUPABASE_API_KEY
-try:
-    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-except Exception as e:
-    print("❌ Error creating Supabase client:", e)
-    
+
+supabase: Optional[Client] = None
+
+if SUPABASE_URL !="" and SUPABASE_KEY != "":
+    try:
+        supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+        print("✅ Supabase client created successfully.")
+    except Exception as e:
+        print(f"❌ Error creating Supabase client: {e}")
+        supabase = None
+else:
+    print(F"[SUPABASE_URL]: {SUPABASE_URL} ", f"[SUPABASE_KEY]: {SUPABASE_KEY}")
+    print("⚠️ SUPABASE_URL o SUPABASE_API_KEY no están configuradas. Cliente Supabase deshabilitado.")
+    supabase = None
+
 if __name__ == "__main__":
     print("Supabase client created successfully.")
     result = supabase.table("dolar").select("*").execute()
